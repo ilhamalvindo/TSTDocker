24aefbebacd0cad561ddab77205796f241fbef53
Ilham Alvindo Riandova <68469047+ilhamalvindo@users.noreply.github.com>
2022-02-10 12:37:55 +0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,9 +6,6 @@ import os
 SQLALCHEMY_DATABASE_URL = "postgresql://xpjfchbo:password@rosie.db.elephantsql.com/xpjfchbo"
 
 
-
-#SQLALCHEMY_DATABASE_URL = os.environ['postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com:5432/xpjfchbo']
-
 #conn = psycopg2.connect(SQLALCHEMY_DATABASE_URL, sslmode='require')
 
 engine = create_engine(
