bdfbd491c789882dd5864b0829c4e903ff22b283
Ilham Alvindo Riandova <68469047+ilhamalvindo@users.noreply.github.com>
2022-02-10 12:37:34 +0700

    update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
+SQLALCHEMY_DATABASE_URL = "postgresql://xpjfchbo:password@rosie.db.elephantsql.com/xpjfchbo"
 
-#"postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
 
 
 #SQLALCHEMY_DATABASE_URL = os.environ['postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com:5432/xpjfchbo']
